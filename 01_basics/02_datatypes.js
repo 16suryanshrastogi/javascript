810bd84be446538559ab38c143f8b32beb03eb52
Suryansh Rastogi <79991872+16suryanshrastogi@users.noreply.github.com>
2024-02-11 13:48:55 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,3 +2,23 @@
 
 // alert(3+3) //using node not browser
 
+
+let name = "suri"
+let age = 22
+let isLoggedIn  = false
+
+// number => 2 ^53
+// bigint
+// string=> ""
+// boolean => true/false
+// null => stanalone value
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
