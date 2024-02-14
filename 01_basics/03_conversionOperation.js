4d359113f54fb69f3d78554e01e3aa0c108e31b1
Suryansh Rastogi <79991872+16suryanshrastogi@users.noreply.github.com>
2024-02-14 13:49:58 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "6hyg"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "6" => 6
+// "6hyg" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "hgg"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hgg" => true
+
