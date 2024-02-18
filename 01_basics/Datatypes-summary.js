931a1f98759cb242234335140f531dfc60397037
Suryansh Rastogi <79991872+16suryanshrastogi@users.noreply.github.com>
2024-02-18 14:59:57 +0000

    Datatype-summary

---
+++ 01_basics/Datatypes-summary.js
@@ -0,0 +1,19 @@
+//primitive datatypes
+// 7 types: String, number, boolean, null, Undefined, Symbol, BigInt
+
+
+
+
+// non primitive or reference types
+// Arrays, Objects, functions
+
+const heros ={ "batman", "superman", " doga"}
+
+let myObje = {
+    name: "suryansh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
